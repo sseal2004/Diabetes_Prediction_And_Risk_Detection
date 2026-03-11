444381e608338fe5d87328cefc301c80bea73245
Soumyadipta Seal <s.seal.a.b.c@gmail.com>
2026-03-11 21:18:25 +0530

    done

--- client/src/components/DiabetesForm.jsx
+++ client/src/components/DiabetesForm.jsx
@@ -27,7 +27,7 @@ const DiabetesForm = ({ setResult, setFormData }) => {
     e.preventDefault();
     setLoading(true);
     try {
-      const res = await axios.post("http://localhost:5000/predict", data);
+      const res = await axios.post("https://diabetes-prediction-and-risk-detection.onrender.com/predict", data);
       setResult(res.data);
       setFormData(data);
     } catch (err) {
